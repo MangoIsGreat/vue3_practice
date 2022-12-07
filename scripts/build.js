45a2cf2ec8ffbc3f4273acc4f0dfaceb39668ef7
钟鹏 <peng.zhong@qlchat.com>
2022-12-08 00:12:07 +0800

    项目文件夹&build.js

---
+++ scripts/build.js
@@ -0,0 +1,11 @@
+// 进行打包 monerepo
+const fs = require("fs");
+
+const dirs = fs.readdirSync("packages").filter((p) => {
+  if (!fs.statSync(`packages/${p}`).isDirectory()) {
+    return false;
+  }
+  return true;
+});
+
+console.log(dirs);
