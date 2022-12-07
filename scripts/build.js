5d662fc66ba0441aa0e9839099dd8295e6b24b53
钟鹏 <peng.zhong@qlchat.com>
2022-12-08 02:00:43 +0800

    并行打包方法

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 // 进行打包 monerepo
 const fs = require("fs");
+const execa = require("execa");
 
+// 一、获取各模块
 const dirs = fs.readdirSync("packages").filter((p) => {
   if (!fs.statSync(`packages/${p}`).isDirectory()) {
     return false;
@@ -9,3 +11,20 @@ const dirs = fs.readdirSync("packages").filter((p) => {
 });
 
 console.log(dirs);
+
+// 二、并行打包
+async function build(target) {
+  await execa("rollup", ["-c", "--environment", `TARGET:${target}`]);
+}
+
+async function runParaller(dirs, itemfn) {
+  let result = [];
+  for (let item of dirs) {
+    result.push(itemfn(item));
+  }
+  return Promise.all(result);
+}
+
+runParaller(dirs, build).then(() => {
+  console.log("成功");
+});
